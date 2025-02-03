80433e1bce92263ae0b25f5c5a1e126e79c04c7b
Caznix <caznix01@gmail.com>
2025-02-03 17:02:18 -0500

    use adapter static instead of auto

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,23 @@
-import adapter from '@sveltejs/adapter-auto';
+
+import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  kit: {
-    adapter: adapter(),
-    alias: {
-      $lib: './src/lib'
-    }
-  },
-};
+	kit: {
+		adapter: adapter({
+			pages: "build",
+			assets: "build",
+			fallback: '404.html',
+			precompress: false,
+			strict: true
+		}),
+		prerender: {
+			entries: ['*'],
+			handleMissingId: 'warn', 
+		}
 
+	},
+	// preprocess: [mdsvex()],
+	extensions: [".svelte", ".svx"]
+};
 export default config;
