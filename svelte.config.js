9dc472385834054c13fbae8420fdc5d9c09ef2e5
Caznix <caznix01@gmail.com>
2025-02-03 17:43:35 -0500

    suppress 404 errors

--- svelte.config.js
+++ svelte.config.js
@@ -14,6 +14,14 @@ const config = {
 		prerender: {
 			entries: ['*'],
 			handleMissingId: 'warn', 
+			handleHttpError: ({ path, referrer, message }) => {
+				// ignore deliberate link to shiny 404 page
+				console.warn(`${path} ${referrer} ${message}`);
+				return;
+				// if (path === '/not-found' && referrer === '/blog/how-we-built-our-404-page') {
+				// 	return;
+				// }
+			}
 		}
 
 	},
